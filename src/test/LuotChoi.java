3d205404baa971ec37374430875286ec9c4a7397
Hieu-alt <dangthanhhieu199@gmail.com>
2025-06-18 03:50:09 +0000

    .

---
+++ src/test/LuotChoi.java
@@ -0,0 +1,17 @@
+public class LuotChoi {
+    private String tenNguoiChoi;
+    private int diem;
+
+    public LuotChoi(String tenNguoiChoi, int diem) {
+        this.tenNguoiChoi = tenNguoiChoi;
+        this.diem = diem;
+    }
+
+    public String getTenNguoiChoi() {
+        return tenNguoiChoi;
+    }
+
+    public int getDiem() {
+        return diem;
+    }
+}
